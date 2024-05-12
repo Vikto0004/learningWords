6227e1a87d86df6d6a8192ed8cc762d7d0a35805
Vikto0004 <150432826+Vikto0004@users.noreply.github.com>
2024-05-12 11:22:45 +0000

    Deploying to gh-pages from @ Vikto0004/learningWords@b98e7e61dccadf5fd24c391cddaba4d38b56e152 🚀

--- assets/render-functions-26b0f92c.js
+++ assets/render-functions-26b0f92c.js
@@ -1,4 +1,4 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const l=i=>{let r="";for(let s=0;s<i;s++)r+=`<div class="add-wrap-item">
+const l=a=>{let t="";for(let s=0;s<a;s++)t+=`<div class="add-wrap-item">
       <div class="add-wrap-img">
         <img class="add-search-img" src="./img/letter-w.png" alt="" />
         <button class="add-wrap-img-icon-left" type="button">
@@ -29,14 +29,14 @@
         aria-label="Sizing example input"
         aria-describedby="inputGroup-sizing-lg"
       />
-    </div>`;return r},n=i=>i.map(({title:r,number:s,progress:a,time:e})=>` <div class="modal-content rounded-4 shadow">
+    </div>`;return t},r=a=>a.map(({title:t,number:s,progress:i,time:e})=>` <div class="modal-content rounded-4 shadow">
       <div class="modal-body p-5">
         <button class="card-btn-delete" type="button">
           <svg width="30px" height="25px">
             <use href="./img/sprite.svg#icon-delete"></use>
           </svg>
         </button>
-        <h2 class="fw-bold mb-0">${r}</h2>
+        <h2 class="fw-bold mb-0">${t}</h2>
 
         <ul class="d-grid gap-4 my-5 list-unstyled small">
           <li class="d-flex gap-4">
@@ -45,7 +45,7 @@
           </li>
           <li class="d-flex gap-4">
             <h5 class="mb-0">Progress:</h5>
-            <p>${a}</p>
+            <p>${i}</p>
           </li>
           <li class="d-flex gap-4">
             <h5 class="mb-0">Execution time:</h5>
@@ -60,5 +60,5 @@
           Start!
         </button>
       </div>
-    </div>`).join("");export{n as a,l as m};
-//# sourceMappingURL=render-functions-eea66af9.js.map
+    </div>`).join("");export{r as a,l as m};
+//# sourceMappingURL=render-functions-26b0f92c.js.map
