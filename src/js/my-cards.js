d7e34e92d0d13fb0dc2f5692a2183b0244d7082e
Vikto0004 <vikto0004@gmail.com>
2024-05-14 12:30:55 +0200

    Update

--- src/js/my-cards.js
+++ src/js/my-cards.js
@@ -1,9 +1,11 @@
 import { markupGetCards } from './render-functions';
+import { markupTaskChoose } from './render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const elWrapTaskes = document.querySelector('.wrap-taskes');
 const elWrapTitle = document.querySelector('.wrap-title');
+const elTaskesContainer = document.querySelector('.taskes-container');
 const arrCardSave = JSON.parse(localStorage.getItem('cardInfoSave')) || [];
 
 if (!arrCardSave.length) {
@@ -14,10 +16,10 @@ if (!arrCardSave.length) {
 
 elWrapTaskes.addEventListener('click', e => {
   const elBtnDelete = e.target.closest('.card-btn-delete');
+  const elWrapItem = e.target.closest('.modal-content');
+  const titleWrap = elWrapItem.querySelector('.fw-bold').textContent;
 
   if (elBtnDelete) {
-    const elWrapItem = elBtnDelete.closest('.modal-content');
-    const titleWrap = elWrapItem.querySelector('.fw-bold').textContent;
     const newArrCardSave = arrCardSave.filter(
       ({ title }) => title !== titleWrap
     );
@@ -27,5 +29,11 @@ elWrapTaskes.addEventListener('click', e => {
       message: `Card ${titleWrap} was deleted`,
       position: 'topRight',
     });
+  } else if (e.target.textContent.trim() === 'Start!') {
+    const currentTask = arrCardSave.filter(({ title }) => title === titleWrap);
+    const { mode, language, time } = JSON.parse(
+      localStorage.getItem('settingsTaskSave')
+    );
+    elTaskesContainer.innerHTML = markupTaskChoose(...currentTask, language);
   }
 });
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -75,3 +75,65 @@ export const markupGetCards = arrCardSave => {
     })
     .join('');
 };
+
+export const markupTaskChoose = (currentTask, language) => {
+  const arrTrueWords = shuffleArray(currentTask[language]);
+
+  let langChoose = 'Translate';
+  if (language === 'Translate') langChoose = 'Original';
+  const arrChooseWords = shuffleArray(currentTask[langChoose]);
+
+  const markup = arrTrueWords.map((trueWord, i, array) => {
+    return `<div class="task-wrap">
+      <div class="task-wrap-top">
+        <p class="task-wrap-top-val">Value</p>
+        <p class="task-wrap-top-count">00 / 00</p>
+      </div>
+      <div class="wrap-word_img">
+        <p class="task-wrap-word">${trueWord}</p>
+        <img
+          class="task-img-word task-img-word-blur"
+          src="${searchImgByWord(trueWord, currentTask, language)}"
+          alt="word"
+        />
+      </div>
+      <p class="task-wrap-text">Choose <span>the</span> appropriate term</p>
+      <ul class="task-list">${mixingItem(trueWord, arrChooseWords)}</ul>
+      <button class="task-text-know" type="button">I don't know</button>
+    </div>`;
+  });
+  return markup.join('');
+};
+
+//* Shuffling array
+function shuffleArray(array) {
+  const newArr = [...array];
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+  return newArr;
+}
+
+function mixingItem(trueWord, array) {
+  let trueItem = `<li class="task-list-item"><button class="task-list-item-btn">${trueWord}</button></li>`;
+  const arrMarkup = [];
+
+  let countet = 0;
+  for (const word of array) {
+    if (countet === 3) break;
+    else if (word !== trueWord) {
+      const el = `<li class="task-list-item"><button class="task-list-item-btn">${word}</button></li>`;
+      arrMarkup.push(el);
+      countet++;
+    }
+  }
+  arrMarkup.push(trueItem);
+  return arrMarkup.join('');
+}
+
+function searchImgByWord(trueWord, currentTask) {
+  const indexWord = currentTask.Original.indexOf(trueWord);
+  const linkImg = currentTask.linkOnImg[indexWord];
+  return linkImg;
+}
